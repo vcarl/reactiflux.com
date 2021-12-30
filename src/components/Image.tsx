74d5c8a3700a260f739e8c2aa2aa90df122c3c44
Carl Vitullo <carl.vitullo@gmail.com>
2021-12-30 15:53:31 -0600

    fork test

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,9 +1,6 @@
-import React, { ImgHTMLAttributes } from "react";
+import React from "react";
+import NextImage, { ImageProps } from "next/image";
 
-// Unfortunately Next doesn't do any image processing for `next export` and
-// blows up if you try to use it. Swap it out for a dead-simple image with no
-// optimization
-export default function Image(props: ImgHTMLAttributes<HTMLImageElement>) {
-  // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
-  return <img {...props} />;
+export default function Image(props: ImageProps) {
+  return <NextImage {...props} />;
 }
